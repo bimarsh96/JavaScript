3e4b91480a0980be591522dc2d828c8ce6adbbd4
bimarsh96 <bimarshmahat99@gmail.com>
2026-05-28 17:25:20 +0545

    no

---
+++ string.js
@@ -0,0 +1,38 @@
+// let name='hari'
+// let age=34;
+
+
+// console.log(`my name is ${name}.my age is ${age}`)
+
+
+// const name= new String (` bimarsh`);
+// console.log(name);
+
+
+
+// console.log(`my name is bimarsh .i live in ${address}`)//
+
+
+// const address = new String (`devinagar`);
+// console.log(address[0]);
+// console.log(address.__proto__);
+// console.log(address.length);
+// console.log(address[4])
+// console.log(address.toUpperCase());
+// console.log(address.charAt(5));
+
+
+const roll=45;
+console.log(roll);
+
+const balance = new Number(500);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+const otherno = 123.995678;
+console.log(otherno.toPrecision(3));
+
+const bank = 10000000;
+console.log(bank.toLocaleString());
