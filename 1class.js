105ffaf94c20c42e08000ccf5b528e2e1879df16
bimarsh96 <bimarshmahat99@gmail.com>
2026-05-28 14:16:26 +0545

    new

--- 1class.js
+++ 1class.js
@@ -1,1 +1,9 @@
-console.log("bimarsh")
+const accountId = "12345" ;
+let name = "bimarsh";
+let rollno = 1;
+var city = "Butwal "
+let phoneNumber ;
+
+name = "Ram";
+console.log(accountId);
+console.table([accountId , name , rollno ,city , phoneNumber]);
---
+++ datatype.js
@@ -0,0 +1,20 @@
+console.log(null > 0)
+console.log(null == 0)
+console.log(null < 0)
+console.log(null >= 0)
+console.log(null <=0 )
+
+
+let a = 5;
+let b= 6;
+let c = a+= 5;
+console.log(c);
+
+let gamecounter=2;
+++gamecounter;
+console.log(gamecounter);
+
+
+let id = Symbol('123');
+let anotherid = Symbol('123');
+console.log(id === anotherid);
