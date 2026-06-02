c9d454062ec0306a43d828202dde318e1e9f7f69
bimarsh96 <bimarshmahat99@gmail.com>
2026-06-02 17:27:31 +0545

    basic

--- function.js
+++ function.js
@@ -14,14 +14,65 @@
 
 
 
-function login (username="ram"){
- if(!username){
-console.log("plese enter a username")
-return
- }
-    
-   
-  return `${username} Just logged in `
-}
+// function login (username="ram"){
+//  if(!username){
+// console.log("plese enter a username")
+// return
+//  }
+// return `${username} Just logged in `
+// }
 
-console.log(login())
+// // console.log(login())
+
+
+
+// function mycart(val1,val2, ...num1){     //VAL1 AND VAL2  to store value..
+// return num1
+// }
+// console.log(mycart(200,400,500))
+
+
+
+// const office={
+//     name:"ramu",
+//     price:400,
+// }
+
+// function room(anyobject){
+//     console.log(`user name is ${anyobject.name} total price is ${anyobject.price}`)
+// }
+// console.log(room(office))
+
+// const newArry=[200,400,100,50]
+
+// function returnArry(getarry){
+//          return getarry[2]
+// }
+// console.log(returnArry(newArry))
+
+//  if(true){
+//     const username="ritesh"
+//     if(username == "ritesh"){
+//         const website = " google"
+//         console.log(username+website);
+
+//     }
+    // console.log(website)
+// }
+// console.log(username)           //because from child to parent cannt be executed bt from parent to child can be
+  
+
+
+// const menu = (num1 ,num2)  => {
+//     return num1+ num2
+// }
+
+// console.log(menu(3,4))
+
+( ()  => {
+   console.log(`hello`)
+})();
+
+( function ()  {
+   console.log(`hello`)
+})()
---
+++ items.js
@@ -0,0 +1,41 @@
+
+
+// for(let i of coding){
+//     console.log(i)
+// }
+
+
+// const coding=["apple","mango","litchi","orange"]
+
+
+// coding.forEach( function (item) {
+  
+// console.log(item)
+// } )
+
+
+// coding.forEach( (item) => {
+// console.log(item)
+// } )
+
+
+const mycoding  = [    
+    {
+        languagename:"javascript",
+        languagefilename:"js"
+    },
+     {
+        languagename:"advanced java",
+        languagefilename:"aj"
+    },
+     {
+        languagename:"python",
+        languagefilename:"p"
+    }
+]
+
+
+mycoding.forEach( (item) => {
+
+    console.log(item.languagename)
+}  )
