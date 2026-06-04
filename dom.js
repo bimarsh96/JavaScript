e44fd2c60ff345a285b01035c6f8fb1da95be7af
bimarsh96 <bimarshmahat99@gmail.com>
2026-06-04 14:53:39 +0545

    event

--- dom.js
+++ dom.js
@@ -1,3 +1,78 @@
 const sec=document.getElementsByClassName(".first").innerHTML="IT changed"
 
 console.log(sec)
+
+
+ <div class="black">
+    <h1 id="title" class="heading"> Old Heading</h1>
+    <h2 id="seclin" class="just">dang dang</h2>
+    <h2 id="seclin" class="just">dang dang</h2>
+    <h2 id="seclin" class="just">dang dang</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea id itaque 
+    </p>
+    <ul>
+        <li> onw</li>
+        <li> tow</li>
+        <li> trhe</li>
+    </ul>
+</div>
+ <script>
+const element = document.getElementById("id01");
+element.innerHTML = "New Heading";
+</script> 
+
+
+
+
+const parent= document.querySelector(".parent");
+console.log(parent)
+console.log(parent.children )
+console.log(parent.children[1].innerHTML )
+for(let i=0; i < parent.children.length ; i++){
+              console.log(parent.children[i].innerHTML);
+}
+
+ parent.children[0].style.color="orange"
+console.log(parent.firstElementChild);
+console.log(parent.lastElementChild);
+
+
+const day=document.querySelector(".day");
+console.log(day)
+console.log(day.parentElement);
+
+const div =document.createElement("div")
+console.log(div)
+div.className="main"
+div.id="optional"
+div.id=Math.random()*10
+
+
+
+
+<body class="main" style="background-color: black; color:white" >
+<ul class="just">
+    <li >JAva</li>
+</ul>
+
+   
+<div class="parent" >
+<div class="day">sunday</div>
+<div class="day">monday</div>
+<div class="day">tuesday</div>
+<div class="day">wednesday</div>
+<div class="day">thursday</div>
+</div>
+
+
+<script>
+function   addlang (langName) {
+    const li= document.createElement("li");
+    li.innerHTML="$(langName)"
+   document.querySelector(".just").
+   appendChild(li)
+  
+}
+ addlang("pythion")
+   addlang("aj")
+</script> 
